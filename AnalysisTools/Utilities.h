605cb0994f9320b9f5b2a9e0ebdcc811ff703db9
asogaard <andreas.sogaard@gmail.com>
2016-10-17 09:18:40 +0200

    Add string trimming utility methods.

--- AnalysisTools/Utilities.h
+++ AnalysisTools/Utilities.h
@@ -9,7 +9,7 @@
 // STL include(s).
 #include <string>
 #include <vector>
-#include <fstream> /* ifstream */
+#include <fstream>
 #include <limits> /* std::numeric_limits<double>::infinity */
 #include <algorithm> /* std::find */
 #include <iterator> /* std::distance */
@@ -22,6 +22,7 @@ namespace AnalysisTools {
     
     const double inf =    std::numeric_limits<double>::infinity();
     const double eps = 1./std::numeric_limits<double>::max();
+    const double pi  = 3.14159265359;
     
     // Check whether file exists.
     inline bool fileExists (const string& filename) {
@@ -56,6 +57,25 @@ namespace AnalysisTools {
             return (0 == full.compare (full.length() - ending.length(), ending.length(), ending));
         }
         return false;
+    }
+
+    // Trim string from start.
+    static inline std::string& ltrim(std::string &s) {
+      s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+				      std::not1(std::ptr_fun<int, int>(std::isspace))));
+      return s;
+    }
+
+    // Trim string from end.
+    static inline std::string& rtrim(std::string &s) {
+      s.erase(std::find_if(s.rbegin(), s.rend(),
+			   std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+      return s;
+    }
+
+    // Trim string from both ends.
+    static inline std::string& trim(std::string &s) {
+      return ltrim(rtrim(s));
     }
     
     // Split a string by delimeter.
