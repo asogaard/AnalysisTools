d8a25ce2db4a1ae0600c0517fb7264f7e5dcb572
asogaard <andreas.sogaard@gmail.com>
2017-05-22 18:09:59 +0200

    Update stats input script

--- scripts/generateStatsHists.py
+++ scripts/generateStatsHists.py
@@ -38,9 +38,8 @@ def main ():
 
     # Specify which variables to get.
     print "-- Specify variables to read."
-    treename = 'BoostedJet+ISRgamma/Tight/EventSelection/Pass/tau21DDT_0p50/Postcut'
-    #treename = 'BoostedJet+ISRgamma/EventSelection/Pass/NumFatjetsAfterDphi/Postcut'
-    prefix   = 'leading_LargeRadiusJets_'
+    treename = 'BoostedJet+ISRgamma/Nominal/EventSelection/Pass/Jet_tau21DDT/Postcut'
+    prefix   = 'Jet_' # 'leading_LargeRadiusJets_'
 
     getvars  = ['m']
 
@@ -58,8 +57,6 @@ def main ():
     print "-- Initialise number of (good) jets."
     N = len(data[getvars[0]])
 
-    lumi = 36.1
-
 
     # Fill output histograms.
     # ----------------------------------------------------------------
@@ -67,45 +64,20 @@ def main ():
 
     hists = list()
     ranges = [
+
         # Backgrounds.
-        #((361039, 361062), "QCD"), # Sherpa gamma + jet
-        ((100080, 100080), "QCD"), # Sherpa gamma + jet
+        ((361039, 361062), "QCD"), # Sherpa gamma + jet
+        #((100080, 100080), "QCD"), # Sherpa gamma + jet
         ((305435, 305439), "W"), # Sherpa gamma + W
         ((305440, 305444), "Z"), # Sherpa gamma + Z
 
         # Signals.
-        #((305159, 305159), "mRp1_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp1_mD10_gSp3_gD1
-        #((305160, 305160), "mRp2_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp2_mD10_gSp3_gD1
-        #((305161, 305161), "mRp3_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp3_mD10_gSp3_gD1
-        #((305162, 305162), "mRp4_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp4_mD10_gSp3_gD1
-        #((305163, 305163), "mRp5_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp5_mD10_gSp3_gD1
-        
-        #((305367, 305367), "mRp015_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp015_mD10_gSp3_gD1
-        #((305368, 305368), "mRp025_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp025_mD10_gSp3_gD1
-        #((305369, 305369), "mRp035_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp035_mD10_gSp3_gD1
-        #((305370, 305370), "mRp045_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp045_mD10_gSp3_gD1
-        #((305371, 305371), "mRp055_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp055_mD10_gSp3_gD1
-        ((305372, 305372), "mRp065_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp065_mD10_gSp3_gD1
-        #((305373, 305373), "mRp075_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp075_mD10_gSp3_gD1
-        #((305374, 305374), "mRp085_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp085_mD10_gSp3_gD1
-        
-        #((305465, 305465), "mRp25_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp25_mD10_gSp3_gD1
-        #((305466, 305466), "mRp25_mD10_gSp2_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp25_mD10_gSp2_gD1
-        #((305467, 305467), "mRp25_mD10_gSp1_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp25_mD10_gSp1_gD1
-        #((305468, 305468), "mRp35_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp35_mD10_gSp3_gD1
-        #((305469, 305469), "mRp35_mD10_gSp2_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp35_mD10_gSp2_gD1
-        #((305470, 305470), "mRp35_mD10_gSp1_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp35_mD10_gSp1_gD1
-        #((305471, 305471), "mRp45_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp45_mD10_gSp3_gD1
-        #((305472, 305472), "mRp45_mD10_gSp2_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp45_mD10_gSp2_gD1
-        #((305474, 305474), "mRp55_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp55_mD10_gSp3_gD1
-        #((305475, 305475), "mRp55_mD10_gSp2_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp55_mD10_gSp2_gD1
-        #((305476, 305476), "mRp55_mD10_gSp1_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp55_mD10_gSp1_gD1
-        #((305477, 305477), "mRp75_mD10_gSp4_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp75_mD10_gSp4_gD1
-        #((305478, 305478), "mRp75_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp75_mD10_gSp3_gD1
-        #((305479, 305479), "mRp75_mD10_gSp2_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp75_mD10_gSp2_gD1
-        #((305481, 305481), "mRp95_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mRp95_mD10_gSp3_gD1
-        #((305482, 305482), "mR1p5_mD10_gSp4_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mR1p5_mD10_gSp4_gD1
-        #((305483, 305483), "mR1p5_mD10_gSp3_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph100_mR1p5_mD10_gSp3_gD1
+        ((308363, 308363), "mRp100_mD10_gSp5_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph150_mRp100_mD10_gSp5_gD1
+        ((308364, 308364), "mRp130_mD10_gSp5_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph195_mRp130_mD10_gSp5_gD1
+        ((308365, 308365), "mRp160_mD10_gSp5_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph240_mRp160_mD10_gSp5_gD1
+        ((308366, 308366), "mRp190_mD10_gSp5_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph285_mRp190_mD10_gSp5_gD1
+        ((308367, 308367), "mRp220_mD10_gSp5_gD1"), # MGPy8EG_N30LO_A14N23LO_dmA_jja_Ph330_mRp220_mD10_gSp5_gD1
+
         ]
 
     # Get titles.
@@ -135,9 +107,6 @@ def main ():
         print "Filling tree for '%s'." % title
         msk = np.where((data['DSID'] >= DSIDs[0]) & (data['DSID'] <= DSIDs[1]))
 
-        # Scale by luminosity
-        data['weight'][msk] *= lumi
-
         # Fill tree
         M = [tuple(el) for el in np.hstack((np.atleast_2d(data['m']     [msk].ravel()).T,
                                             np.atleast_2d(data['weight'][msk].ravel()).T)).tolist()]
@@ -159,11 +128,6 @@ def main ():
         print "Filling tree for '%s'." % title
         msk = np.where((data['DSID'] >= DSIDs[0]) & (data['DSID'] <= DSIDs[1]))
         
-        # Scale by luminosity
-        if title in ['W', 'Z']:
-            data['weight'][msk] *= lumi
-            pass
-
         # Fill tree
         M = [tuple(el) for el in np.hstack((np.atleast_2d(data['m']     [msk].ravel()).T,
                                             np.atleast_2d(data['weight'][msk].ravel()).T)).tolist()]
@@ -176,8 +140,6 @@ def main ():
         pass
 
     backgroundfile.Close()
-
-    # ...
 
     return
 
